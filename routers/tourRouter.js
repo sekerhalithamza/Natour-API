5fe6014f0b7fa9511d0bac64e1ac0cb62f7da729
Halit Hamza Şeker <shalithamza@gmail.com>
2023-09-10 13:52:15 +0300

    Updating getTour and getAllTours methods

--- routers/tourRouter.js
+++ routers/tourRouter.js
@@ -1,17 +1,39 @@
 const express = require("express");
 const mongoose = require("mongoose");
 
-const getAllTours = (req, res) => {
-  res.status(200).json({
-    status: "success",
-    results: tours.length,
-    data: {
-      tours,
-    },
-  });
+const getAllTours = async (req, res) => {
+  try {
+    const tours = await Tour.find();
+    res.status(200).json({
+      status: "success",
+      results: tours.length,
+      data: {
+        tours,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: "failed",
+      message: err.message,
+    });
+  }
 };
 
-const getTour = (req, res) => {};
+const getTour = async (req, res) => {
+  try {
+    const tour = await Tour.findById(req.params.id);
+
+    res.status(200).json({
+      status: "success",
+      data: tour,
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "failed",
+      message: err.message,
+    });
+  }
+};
 
 const createTour = async (req, res) => {
   // const newTour = new Tour({});
