63fce281e5ad09088f3fadde253393856d95c1df
Halit Hamza Şeker <shalithamza@gmail.com>
2023-09-10 13:24:03 +0300

    Implating database mongodb

--- app.js
+++ app.js
@@ -4,6 +4,56 @@ const dotenv = require("dotenv");
 dotenv.config({ path: `./config.env` });
 const express = require("express");
 const morgan = require("morgan");
+// eslint-disable-next-line import/no-extraneous-dependencies
+const mongoose = require("mongoose");
+
+// DATABASE
+const DB = process.env.DATABASE.replace(
+  "<USERNAME>",
+  process.env.DATABASE_USER_NAME
+).replace("<PASSWORD>", process.env.DATABASE_PASSWORD);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+  })
+  .then(() => {
+    console.log("DB connection succesful");
+  });
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "A tour must have a name"],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 0,
+  },
+  price: {
+    type: Number,
+    required: [true, "A tour must have a price"],
+  },
+});
+
+const Tour = mongoose.model("Tour", tourSchema);
+
+const testTour = new Tour({
+  name: "The Park Camper",
+  price: 997,
+});
+
+testTour
+  .save()
+  .then(doc => {
+    console.log(doc);
+  })
+  .catch(err => {
+    console.log(err.message);
+  });
+
+// THE APP
 
 // Creating the app
 const app = express();
@@ -25,5 +75,6 @@ app.use("/api/v1/users", userRouter);
 // Starting the server
 const port = process.env.port || 3000;
 app.listen(port, () => {
+  // eslint-disable-next-line no-console
   console.log(`App running on port ${port}...`);
 });
